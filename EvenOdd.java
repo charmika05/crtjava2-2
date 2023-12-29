4ab0480b9f1e4b1d2ef2f0b3823d08af7862d2a9
charmika05 <155128932+charmika05@users.noreply.github.com>
2023-12-29 14:22:02 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class EvenOdd
+{
+	
+public static void main(String[] args) 
+	
+{
+	    
+Scanner s=new Scanner(System.in);
+	    
+System.out.println("Enter the numbers ");
+	    
+int a;
+	    
+a=s.nextInt();
+	   
+if(a%2==0)
+	   
+{
+	    
+System.out.println(a+"is even number");
+	   
+}
+	   
+else
+	   
+{
+	       
+System.out.println(a+" is odd numberr");
+	   
+}
+	
+}
+
+}
