6721aae345f7e65e1f9f1cf75714e751e62d5166
charmika05 <155128932+charmika05@users.noreply.github.com>
2023-12-29 14:22:36 +0530

    Add files via upload

---
+++ NestedIf.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class NestedIf
+
+{
+	
+public static void main(String[] args) 
+
+{
+	    
+Scanner s=new Scanner(System.in);
+	    
+System.out.println("Enter the numbers that need to be compared ");
+	    
+int a,b,c;
+	    
+a=s.nextInt();
+	    
+b=s.nextInt();
+	    
+c=s.nextInt();
+	    
+if(a>b)
+	    
+{
+	        
+if(a>c)
+	        
+{
+	            
+System.out.println(a+" is the biggest");
+	            
+	        
+}
+	        
+else
+	        
+{
+	            
+System.out.println(b+" is tyhe biggest");
+	        
+}
+	   
+}
+	    
+else
+	    
+if(b>c)
+	            
+{
+	                
+System.out.println(b+" is the biggest");
+	                
+	            
+}
+	            
+else
+	            
+{
+	                
+System.out.println(c+" is the biggest");
+	           
+}
+
+}
+}	    
