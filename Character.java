a101964def47a9a1dfb13cfb46a007ca36bffed9
charmika05 <155128932+charmika05@users.noreply.github.com>
2023-12-29 14:57:55 +0530

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Character
+{
+	
+public static void main(String[] args) 
+	
+{
+	    
+Scanner s=new Scanner(System.in);
+	    
+System.out.println("Enter the Character ");
+	    
+char a;
+	    
+a=s.next().charAt(0);
+	   
+if(a=='a' || a=='e' || a=='i' || a=='o' || a=='u'|| a=='A' || a=='E' || a=='I' || a=='O' || a=='U')
+	    
+{
+	        
+System.out.println("vowel");
+	        
+	    
+}
+	    
+else
+	   
+{
+	      
+System.out.println("consonant");
+	    
+}
+
+	
+}
+
+}
