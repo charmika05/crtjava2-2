8b27dd3a5481ae22adcb0f9fa81eac0f1e2364ee
charmika05 <155128932+charmika05@users.noreply.github.com>
2023-12-30 14:07:21 +0530

    Add files via upload

---
+++ While1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class While2
+{
+	
+public static void main(String[] args) 
+	
+{
+	    
+int i=10;
+	    
+while(i>=1)
+	    
+{
+	        
+System.out.print(i+" ");
+	        
+i--;
+	        
+	    
+}
+	
+	
+}
+
+}
